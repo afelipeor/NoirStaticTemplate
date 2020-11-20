733565037c2b4990841edc465b61d58a4caa5b3a
André Felipe <afelipeor@gmail.com>
2020-11-20 14:49:15 -0300

    Added main component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { NavigationSidebarComponent } from "./navigation-sidebar/navigation-sidebar.component";
-import { DashboardComponent } from "./dashboard/dashboard.component";
+import { DashboardComponent } from "./subcomponents/main/dashboard/dashboard.component";
 import { DetailsSidebarComponent } from "./details-sidebar/details-sidebar.component";
 import { TransactionsByDateComponent } from "./subcomponents/details/transactions-by-date/transactions-by-date.component";
 import { SearchBarComponent } from "./subcomponents/main/search/search-bar/search-bar.component";
+import { MainComponent } from "./main/main.component";
 
 @NgModule({
 	declarations: [
@@ -17,6 +18,7 @@ import { SearchBarComponent } from "./subcomponents/main/search/search-bar/searc
 		DetailsSidebarComponent,
 		TransactionsByDateComponent,
 		SearchBarComponent,
+		MainComponent,
 	],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
