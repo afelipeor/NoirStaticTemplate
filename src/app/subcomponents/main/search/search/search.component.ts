403b1280c0dd6b06db19e99576615a1776503eca
André Felipe <afelipeor@gmail.com>
2020-11-20 21:09:26 -0300

    Creates search component

--- src/app/subcomponents/main/search/search/search.component.ts
+++ src/app/subcomponents/main/search/search/search.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { SafeHtml } from "@angular/platform-browser";
+import { Component, OnInit } from "@angular/core";
+import Utils from "../../../../../utils/utils";
+import icons from "../../../../../constants/icon-constants";
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+	selector: "app-search",
+	templateUrl: "./search.component.html",
+	styleUrls: ["./search.component.scss"],
 })
 export class SearchComponent implements OnInit {
+	public readonly searchIcon: SafeHtml = this.utils.makeSvgCodeSafe(
+		icons.SEARCH
+	);
 
-  constructor() { }
+	constructor(private utils: Utils) {}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
