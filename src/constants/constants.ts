05017390858b42adf9fe4170fe21c70bec62e35b
André Felipe <afelipeor@gmail.com>
2020-11-23 19:16:04 -0300

    Adds the Icons

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -4,10 +4,14 @@ import icons from "./icon-constants";
 const constants = {
 	MAIN_MENU: [
 		new MainMenuDto("Dashboard", icons.DASHBOARD_ICON, "/dashboard"),
-		new MainMenuDto("Wallet", icons.DASHBOARD_ICON, "/wallet"),
-		new MainMenuDto("Transactions", icons.DASHBOARD_ICON, "/transaction"),
-		new MainMenuDto("Profile", icons.DASHBOARD_ICON, "/profile"),
-		new MainMenuDto("Payments", icons.DASHBOARD_ICON, "/payment"),
+		new MainMenuDto("Wallet", icons.WALLET_ICON, "/wallet"),
+		new MainMenuDto(
+			"Transactions",
+			icons.TRANSACTIONS_ICON,
+			"/transaction"
+		),
+		new MainMenuDto("Profile", icons.PROFILE_ICON, "/profile"),
+		new MainMenuDto("Payments", icons.PAYMENTS_ICON, "/payment"),
 	],
 };
 export default constants;
