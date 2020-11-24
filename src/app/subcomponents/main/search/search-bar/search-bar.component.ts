336ccfd2e516970f6d43f72f0c4ee6081829d86d
André Felipe <afelipeor@gmail.com>
2020-11-24 15:08:02 -0300

    styles svgs

--- src/app/subcomponents/main/search/search-bar/search-bar.component.ts
+++ src/app/subcomponents/main/search/search-bar/search-bar.component.ts
@@ -1,4 +1,7 @@
+import Utils from "../../../../../utils/utils";
+import { SafeHtml } from "@angular/platform-browser";
 import { Component, OnInit } from "@angular/core";
+import icons from "../../../../../constants/icon-constants";
 
 @Component({
 	selector: "app-search-bar",
@@ -6,7 +9,14 @@ import { Component, OnInit } from "@angular/core";
 	styleUrls: ["./search-bar.component.scss"],
 })
 export class SearchBarComponent implements OnInit {
-	constructor() {}
+	public readonly settingsIcon: SafeHtml = this.utils.makeSvgCodeSafe(
+		icons.SETTINGS_ICON
+	);
+	public readonly hamburgerIcon: SafeHtml = this.utils.makeSvgCodeSafe(
+		icons.HAMBURGER_ICON
+	);
+
+	constructor(private utils: Utils) {}
 
 	ngOnInit(): void {}
 }
